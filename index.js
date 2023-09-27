1e527f0632d540a9a0d2e141e9b78727e39f1123
skwasim777 <wasimsk6061@gmail.com>
2023-09-27 21:07:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const searchBarContainer = document.querySelector(".search-bar-container");
+const magnifierEl = document.querySelector(".magnifier");
+magnifierEl.addEventListener("click", () => {
+  searchBarContainer.classList.toggle("active");
+});
